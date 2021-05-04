4f01d3d88d838635346c95e5f8d9c8c541fd2230
Kirk Lincoln <cohomology@Kirks-MacBook-Pro.local>
2021-05-04 18:13:24 -0500

    code

--- js/index.js
+++ js/index.js
@@ -3,27 +3,52 @@ document.addEventListener("load", () => {
     document.readyState === "complete" ? null : alert("document did not load correctly");
 });
 
-const navLink = document.querySelector(".nav");
-navLink.addEventListener("mouseover", event => {
-    event.target.style.color = "red";
+document.addEventListener("keydown", () => {
+    document.getElementsByClassName("content-section")[0].style.background = "orange";
 });
-navLink.addEventListener("mouseout", event => {
-    event.target.style.color = "";
-})
 
-document.querySelector(".content-section").addEventListener("mouseover", event => {
-    event.target.style.background = "orange";
+document.addEventListener("keyup", event => {
+    document.getElementsByClassName("content-section")[0].style.background = "white";
 })
-document.querySelector(".content-section").addEventListener("mouseout", event => {
-    event.target.style.background = "white";
-})
-
-
 
 document.addEventListener("mouseover", event => {
-    event.target.localName === "img" ? event.target.style.width = "200%" : null;
+    event.target.localName === "img" ? event.target.style.width = "100%" : null;
 });
 
 document.addEventListener("mouseout", event => {
     event.target.localName === "img" ? event.target.style.width = "" : null;
 });
+
+document.addEventListener("scroll", event => {
+    event.target.body.style.lineHeight = 2;
+});
+
+window.addEventListener("resize", () => {
+    document.body.style.width = "400px";
+});
+
+document.addEventListener("dblclick", event => {
+    event.target.style.marginLeft = "10%";
+});
+
+document.addEventListener("keydown", event => {
+    event.key === "Control" ? document.addEventListener("mouseover", mEvent => {
+        mEvent.target.style.marginLeft === "10%" ? 
+        mEvent.target.style.marginLeft = "" :
+        null;
+    }) :
+    null
+});
+
+document.addEventListener("click", event => {
+    const signUp = [...event.target.classList].filter(x => x === "btn")
+    console.log(signUp)
+    signUp.length > 0 ? 
+    (event.target.style.color === "red" ? 
+     event.target.style.color =   ""    : 
+     event.target.style.color =   "red")
+    : null;
+})
+
+
+
